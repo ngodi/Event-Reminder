d33d8761636b7f39e863661bd7ca237cbca0dae1
ngodi <albertngodi@gmail.com>
2018-11-27 07:05:52 +0100

    modified event logo,synchronized timer with days to go

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 // Display the result in the element with id ="demo"
 document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
+document.getElementById("demo1").innerHTML = "In " + days + " Days";
 //if the count down is finished write some text
 
 if(distance < 0){
